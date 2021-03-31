7267f36326fd5cd233764d7679e366b11b571ded
Dishank <dkhatri@splunk.com>
2021-03-31 18:39:53 +0530

    Updated ConfigurationTable.jsx

--- splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/components/ConfigurationTable.jsx
+++ splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/components/ConfigurationTable.jsx
@@ -8,6 +8,34 @@ import TableWrapper from './table/TableWrapper';
 import EntityModal from './EntityModal';
 import { MODE_CREATE } from '../constants/modes';
 
+// NOTE: if bundle script is put some dir instead of js/build, this function will broken.
+export function getBuildDirPath() {
+    const scripts = document.getElementsByTagName('script');
+    const scriptsCount = scripts.length;
+    for (let i = 0; i < scriptsCount; i+=1) {
+        const s = scripts[i];
+        if(s.src && s.src.match(/js\/build/)) {
+            const lastSlashIndex = s.src.lastIndexOf('/');
+            return s.src.slice(0, lastSlashIndex);
+        }
+    }
+    return '';
+}
+
+// NOTE: The resolve will only be executed if the globalConfig exist
+export function loadGlobalConfig() {
+    // Get the configuraiton json file in sync mode
+    return new Promise((resolve, reject) => {
+        fetch(`${getBuildDirPath()}/globalConfig.json`).then((res) => {
+            return res.json();     
+        }).then((json) => {
+            // window.__globalConfig = json;
+            resolve(json);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
 function ConfigurationTable({ serviceName, serviceTitle }) {
     const [open, setOpen] = useState(false);
     const serviceLabel = `Add ${serviceTitle}`;
