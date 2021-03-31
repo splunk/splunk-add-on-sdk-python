e030585226e0563db2da4357dd47779ca8a020b7
Dishank <dkhatri@splunk.com>
2021-03-31 10:21:28 +0530

    updated script.js

--- splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/util/script.js
+++ splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/util/script.js
@@ -1,26 +1,12 @@
-import { axiosCallWrapper } from '../util/axiosCallWrapper';
-// NOTE: The resolve will only be executed if the globalConfig exist
-export function loadGlobalConfig() {
-    // Get the configuraiton json file in sync mode
-    return new Promise((resolve, reject) => {
-        axiosCallWrapper(`${getBuildDirPath()}/globalConfig.json`).then((res) => {
-            return res.json();     
-        }).then((json) => {
-            // window.__globalConfig = json;
-            resolve(json);
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
+import axios from 'axios';
 
 // NOTE: if bundle script is put some dir instead of js/build, this function will broken.
 export function getBuildDirPath() {
     const scripts = document.getElementsByTagName('script');
     const scriptsCount = scripts.length;
-    for (let i = 0; i < scriptsCount; i++) {
+    for (let i = 0; i < scriptsCount; i += 1) {
         const s = scripts[i];
-        if(s.src && s.src.match(/js\/build/)) {
+        if (s.src && s.src.match(/js\/build/)) {
             const lastSlashIndex = s.src.lastIndexOf('/');
             return s.src.slice(0, lastSlashIndex);
         }
@@ -28,3 +14,10 @@ export function getBuildDirPath() {
     return '';
 }
 
+// NOTE: The resolve will only be executed if the globalConfig exist
+export function loadGlobalConfig() {
+    // Get the configuraiton json file in sync mode
+    return axios.get(`${getBuildDirPath()}/globalConfig.json`).then((res) => {
+        return res.data;
+    });
+}
