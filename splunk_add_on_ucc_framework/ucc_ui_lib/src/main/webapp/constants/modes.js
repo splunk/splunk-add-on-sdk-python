57a4f8b486d3393af6c0cd4cdf1c0870423e562a
dkhatri-crest <69460610+dkhatri-crest@users.noreply.github.com>
2021-04-05 09:35:32 +0530

    Update modes.js

--- splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/constants/modes.js
+++ splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/constants/modes.js
@@ -3,3 +3,32 @@ export const MODE_CREATE = 'create';
 export const MODE_DELETE = 'delete';
 export const MODE_EDIT = 'edit';
 export const MODE_CONFIG = 'config';
+
+// NOTE: if bundle script is put some dir instead of js/build, this function will broken.
+export function getBuildDirPath() {
+    const scripts = document.getElementsByTagName('script');
+    const scriptsCount = scripts.length;
+    for (let i = 0; i < scriptsCount; i+=1) {
+        const s = scripts[i];
+        if(s.src && s.src.match(/js\/build/)) {
+            const lastSlashIndex = s.src.lastIndexOf('/');
+            return s.src.slice(0, lastSlashIndex);
+        }
+    }
+    return '';
+}
+
+// NOTE: The resolve will only be executed if the globalConfig exist
+export function loadGlobalConfig() {
+    // Get the configuraiton json file in sync mode
+    return new Promise((resolve, reject) => {
+        fetch(`${getBuildDirPath()}/globalConfig.json`).then((res) => {
+            return res.json();     
+        }).then((json) => {
+            // window.__globalConfig = json;
+            resolve(json);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
