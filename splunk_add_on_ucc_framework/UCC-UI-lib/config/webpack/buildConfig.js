603c2ec26533584f365c338bc14ae98a8df6d5d2
Addon Factory template <addonfactory@splunk.com>
2021-05-07 23:25:26 -0400

    Update buildConfig.js

--- splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
+++ splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
@@ -4,17 +4,17 @@ const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlu
 module.exports = {
     devtool: 'null',
     plugins: [
-        new LicenseWebpackPlugin({
-            outputFilename: '[name].[hash].licenses.txt',
-            perChunkOutput: true,
-            addBanner: true
-        }),
         new webpack.optimize.CommonsChunkPlugin('common.js'),
         new webpack.DefinePlugin({
             __CONFIG_FROM_FILE__: false
         }),
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false }
+        }),
+        new LicenseWebpackPlugin({
+            outputFilename: '[name].[hash].licenses.txt',
+            perChunkOutput: true,
+            addBanner: true
         })
     ]
 };
