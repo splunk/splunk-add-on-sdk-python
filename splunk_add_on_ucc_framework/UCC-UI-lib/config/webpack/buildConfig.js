c7f3b4fa67d43963129d20bd9fd84070c1851476
Addon Factory template <addonfactory@splunk.com>
2021-05-07 22:49:09 -0400

    test

--- splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
+++ splunk_add_on_ucc_framework/UCC-UI-lib/config/webpack/buildConfig.js
@@ -1,14 +1,16 @@
 var webpack = require('webpack');
+var LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
 
 module.exports = {
     devtool: 'null',
     plugins: [
         new webpack.optimize.CommonsChunkPlugin('common.js'),
+        new LicenseWebpackPlugin(),
         new webpack.DefinePlugin({
             __CONFIG_FROM_FILE__: false
         }),
         new webpack.optimize.UglifyJsPlugin({
-            compress: {warnings: false}
+            compress: { warnings: false }
         })
     ]
 };
